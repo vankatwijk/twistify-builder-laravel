0ddf6a465e2dd89ba3c564c5eebfc8cdf6adac46
vankatwijk <hendrikus.hpvk@gmail.com>
2026-03-06 14:28:59 +0100

    Refactor root route to include request handling: Add logic to serve different views based on the host.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\InstasitePreviewController;
 use App\Http\Controllers\TenantSiteController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request, TenantSiteController $tenantSiteController) {
+    $builderHost = strtolower(config('instasites.builder_host', 'builder.twistify.io'));
+    $host = strtolower($request->getHost());
+
+    if ($host === $builderHost) {
+        return view('welcome');
+    }
+
+    return $tenantSiteController->serve($request, '');
 });
 
 Route::get('/test/{host}/{path?}', [InstasitePreviewController::class, 'show'])
